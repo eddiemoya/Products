0965bb5089cdd92a619c2b7bda3403260b97772d
Dan Crimmins <dbcrimmins@gmail.com>
2013-01-31 08:40:32 -0600

    moddified settings view

--- views/admin/settings.php
+++ views/admin/settings.php
@@ -1,6 +1,6 @@
 <div>
 	<h2>SK Products Plugin Settings</h2>
-	<?php echo $settings_field;?>
+	
 	<form action="options.php" method="post">
 	<?php settings_fields($settings_field); ?>
 	<?php do_settings_sections($settings_section); ?>
