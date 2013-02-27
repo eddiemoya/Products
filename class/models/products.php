c4e8b887dc91e6917319a525bbfef8247b057529
Dan Crimmins <dbcrimmins@gmail.com>
2013-02-27 11:56:42 -0600

    model stuff

--- class/models/products.php
+++ class/models/products.php
@@ -2,8 +2,25 @@
 
 class Products_Model {
 	
+	protected $_meta;
+	
 	public function __construct() {
 		
 	}
+	
+	public function meta($name = null, $value = null) {
+		
+		if(($name !== null && ! is_array($name)) && $value === null) {
+			
+		}
+		
+		if($name !== null && $value !== null) {
+			
+		}
+		
+		if($name !== null && is_array($name) )
+	}
+	
+	
 	
 }
