aa71e73198a3fb88e63a38bc7d82b30a7f9baef6
Dan Crimmins <dbcrimmins@gmail.com>
2013-02-28 10:35:45 -0600

    finished products model

--- class/models/products.php
+++ class/models/products.php
@@ -4,22 +4,55 @@ class Products_Model {
 	
 	protected $_meta;
 	
+	protected $_post_args;
+	
 	public function __construct() {
 		
+		$this->post_args('post_type', SHC_PRODUCTS_POSTTYPE);
+	}
+	
+	public static function factory() {
+		
+		return new Products_Model;
 	}
 	
 	public function meta($name = null, $value = null) {
 		
+		return $this->_data('_meta', $name, $value);
+	}
+	
+	public function post_args($name = null, $value = null) {
+		
+		return $this->_data('_post_args', $name, $value);
+	}
+	
+	public function save() {
+		
+		if(! is_wp_error($id = wp_insert_post($this->_post_args, true))) {
+			
+			foreach($this->_meta as $key=>$value) {
+				
+				update_post_meta($id, $key, $value);
+			}
+			
+			return true;
+		}
+			
+		return false;
+	}
+	
+	protected function _data($property, $name = null, $value = null) {
+		
 		//Getter
 		if(($name !== null && ! is_array($name)) && $value === null) {
 			
-			 return $this->_meta[$name];
+			 return $this->{$property}[$name];
 		} 
 		
 		//Setter of single meta
 		if($name !== null && $value !== null) {
 			
-			$this->_meta[$name] = $value;
+			$this->{$property}[$name] = $value;
 		}
 		
 		//Setter for multiples
@@ -27,15 +60,11 @@ class Products_Model {
 			
 			foreach($name as $key=>$value) {
 				
-				$this->_meta[$name] = $value;
+				$this->{$property}[$name] = $value;
 			}
 		}
 		
 		return $this;
 	}
-	
-	
-	
-	
-	
+		
 }
