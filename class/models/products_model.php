40edcd45db30e61a8326dbef09f96df65fd46530
Dan Crimmins <dbcrimmins@gmail.com>
2013-04-02 16:56:30 -0500

    finished products_model

--- class/models/products_model.php
+++ class/models/products_model.php
@@ -6,14 +6,20 @@ class Products_Model {
 	
 	protected $_post_args;
 	
-	public function __construct() {
+	public $products;
+	
+	public $not_found;
+	
+	public function __construct($partnumber = null) {
 		
 		$this->post_args('post_type', SHC_PRODUCTS_POSTTYPE);
+		
+		if($partnumber) $this->get($partnumber);
 	}
 	
-	public static function factory() {
+	public static function factory($partnumber = null) {
 		
-		return new Products_Model;
+		return new Products_Model($partnumber);
 	}
 	
 	public function meta($name = null, $value = null) {
@@ -65,6 +71,135 @@ class Products_Model {
 		return ($wpdb->get_results($q)) ? true : false;
 	}
 	
+	public function get($partnumber) {
+		
+		if(is_array($partnumber)) {
+			
+			$this->_get_multiple($partnumber);
+			
+		} else {
+			
+			$this->post_args(array('meta_query'	=> array(array('key'	=> 'partnumber',
+																'value'	=> $partnumber)),
+									'post_status'	=> 'publish'));
+									
+			$this->products = get_posts($this->_post_args);
+			$this->_add_meta();
+		}
+		
+		return $this;
+	}
+	
+	public function get_by_id($post_id) {
+		
+		if(is_array($post_id)) {
+			
+			$this->_get_multiple_by_id($post_id);
+			
+		} else {
+			
+			$this->products = array(get_post($post_id));
+			$this->_add_meta();
+		}
+		
+		return $this;
+	}
+
+	protected function _get_multiple($partnumber) {
+		
+		foreach($partnumber as $pid) {
+			
+			$this->post_args(array('meta_query'		=> array(array('key'	=> 'partnumber',
+																	'value'	=> $pid)),
+									'post_status'	=> 'publish'));
+			
+			$posts = get_posts($this->_post_args);
+			
+			if(! $posts) {
+				
+				//try import
+				$this->_import($pid);
+				
+			} else {
+				
+				$this->products[] = $posts[0];
+			}
+			
+		}
+		
+		$this->_add_meta();
+	}
+	
+	protected function _get_multiple_by_id($post_id) {
+		
+		foreach($post_id as $id) {
+			
+			$post = get_post($id);
+			
+			if($post) {
+				
+				$this->products[] = $post;
+			}
+		}
+		
+		$this->_add_meta();
+	}
+	
+	protected function _import($partnumber) {
+		
+		$importer = new Product_Importer((array)$partnumber);
+		$importer->import();
+		
+		if($importer->num_products_fail_import) {
+			
+			//if import failed
+			$this->not_found[] = $partnumber;
+			
+		} else {
+			
+			//import was successful
+			$product = Products_Model::factory($partnumber)->products[0];
+			$this->products[] = $product;
+		}
+	}
+	
+	protected function _add_meta() {
+		
+		if($this->products) {
+			
+			foreach($this->products as $key=>$product) {
+				
+				$meta = $this->_convert_meta(get_post_meta($product->ID));
+				
+				$this->products[$key]->meta = $meta;
+				
+				unset($meta);
+			}
+		}
+	}
+	
+	protected function _convert_meta($meta) {
+		
+		$data = new stdClass();
+	
+		foreach((array) $meta as $key=>$meta_value) {
+			
+			$value = @unserialize($meta_value[0]);
+			
+			if($value !== false) {
+				
+				$data->{$key} = $value;
+				
+			} else {
+				
+				$data->{$key} = $meta_value[0];
+			}
+		}
+		
+		return $data;
+	}
+	
+	
 	protected function _data($property, $name = null, $value = null) {
 		
 		//Getter
@@ -87,7 +222,7 @@ class Products_Model {
 				$this->{$property}[$key] = $value;
 			}
 		}
-		
+			
 		return $this;
 	}
 		
